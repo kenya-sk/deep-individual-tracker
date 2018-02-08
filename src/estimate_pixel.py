e31c7fe3982209f8e3886b71c85ae64c59cf582f
kenya-sk <kenya0sakka@gmail.com>
2018-02-08 15:55:13 +0900

    add plot_estimation_box

--- src/estimate_pixel.py
+++ src/estimate_pixel.py
@@ -76,6 +76,7 @@ def estimate():
 
 
 def clustering(densMap, bandwidth, thresh=0):
+    # plot sample point and centroid
     def plot_cluster(X, cluster_centers, labels, n_clusters):
         plt.figure()
         plt.scatter(X[:, 0],X[:,1], c=labels)
@@ -104,9 +105,24 @@ def clustering(densMap, bandwidth, thresh=0):
 
     return centroid_arr
 
+def plot_estimation_box(centroid_arr, boxSize=12):
+    print("cluster: {}".format(len(centroid_arr)))
+    plt.figure(figsize=(12.8, 7.2))
+    ax = plt.axes()
+    centroid_X = centroid_arr[:, 0]
+    centroid_Y = centroid_arr[:, 1] + 250
+    plt.scatter(centroid_X, centroid_Y, s=10)
+    for x, y in zip(centroid_X, centroid_Y):
+        r = plt.Rectangle(xy=(x-boxSize/2, y-boxSize/2), width=boxSize, height=boxSize, ec='#000000', fill=False, edgecolor='red')
+        ax.add_patch(r)
+    plt.xlim(0, 1280)
+    plt.ylim(0, 720)
+    plt.savefig("./estimateBox.png")
+
 
 if __name__ == "__main__":
     estDensMap = estimate()
     np.save("./estimation/2018_2_6_13_34/estimation.npy")
     #estDensMap = np.load("./estimation.npy")
-    #centroid_arr = clustering(estDensMap, 5, 0)
+    centroid_arr = clustering(estDensMap, 5, 0)
+    plot_estimation_box(centroid_arr, 12)
