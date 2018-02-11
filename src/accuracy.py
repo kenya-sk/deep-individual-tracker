527798e5028d4c36dc34e4be62e0b15674e461d2
kenya-sk <kenya0sakka@gmail.com>
2018-02-12 01:29:24 +0900

    add: accuracy

--- src/accuracy.py
+++ src/accuracy.py
@@ -3,14 +3,17 @@
 
 import numpy as np
 import pandas as ps
+from munkres import Munkres
 
 def get_groundTruth(groundTruthPath):
     groundTruth_df = pd.read_csv(groundTruthPath)
-    groundTruth_arr = np.array(df)
+    groundTruth_arr = np.array(groundTruth_df)
 
     return groundTruth_arr
 
-def accuracy(estCentroid_arr, groundTruth_arr):
+
+def accuracy(estCentroid_arr, groundTruth_arr, distTreshold):
+# distance of estimation and groundtruth is less than distThreshold --> True
     def get_norm(x, y):
         return (x**2 + y**2)**0.5
 
@@ -23,13 +26,27 @@ def accuracy(estCentroid_arr, groundTruth_arr):
             distY = estCentroid_arr[i][1] - groundTruth_arr[j][1]
             distMatrix[i][j] = abs(get_norm(distX, distY))
 
-    
+    # munkres algorithm
+    indexes = Munkres().compute(distMatrix)
+    validIndexLength = min(len(estCentroid_arr), len(groundTruth_arr))
+    valid_lst = [i for i in range(validIndexLength)]
+    if len(estCentroid_arr) >= len(groundTruth_arr):
+        matchIndexes = list(filter((lambda index : index[1] in valid_lst), indexes))
+    else:
+        matchIndexes = list(filter((lambda index : index[0] in valid_lst), indexes))
 
+    trueCount = 0
+    for i in range(len(matchIndexes)):
+        estIndex = matchIndexes[i][0]
+        truthIndex = matchIndexes[i][1]
+        if distMatrix[estIndex][truthIndex] < distTreshold:
+            trueCount += 1
 
-
+    accuracy = trueCount / n
+    print("Accuracy: {}".format(accuracy))
 
 
 if __name__ = "__main__":
     estCentroid_arr = np.load("")
     groundTruth_arr = get_groundTruth("")
-    accuracy(estCentroid_arr, groundTruth_arr)
+    accuracy(estCentroid_arr, groundTruth_arr, 6)
