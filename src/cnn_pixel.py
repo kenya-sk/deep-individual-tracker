e9af4c405bbf2037cee3ec4ab8c558df6212f36f
kenya-sk <kenya0sakka@gmail.com>
2018-02-20 13:51:51 +0900

    modify: image size

--- src/cnn_pixel.py
+++ src/cnn_pixel.py
@@ -163,7 +163,7 @@ def main(X_train, X_test, y_train, y_test):
     with tf.name_scope("input"):
         # input image
         with tf.name_scope("X"):
-            X = tf.placeholder(tf.float32, [None, 72, 2, 3], name="input")
+            X = tf.placeholder(tf.float32, [None, 72, 72, 3], name="input")
             _ = tf.summary.image("X(input)", X[:, :, :, 0:1], 5)
         # answer image
         with tf.name_scope("y_"):
@@ -321,7 +321,7 @@ def main(X_train, X_test, y_train, y_test):
     for epoch in range(n_epochs):
         print("elapsed time: {0:.3f} [sec]".format(time.time() - startTime))
         for i in range(len(X_train)):
-            train_df = get_local_data(X_train[i], y_train[i], 36)
+            train_df = get_local_data(X_train[i], y_train[i], 72)
             train_df = under_sampling(train_df, thresh=0.005)
             X_train_local = train_df["img_arr"]
             y_train_local = train_df["label"]
@@ -337,14 +337,14 @@ def main(X_train, X_test, y_train, y_test):
                     print("traning data: {0} / {1}".format(i, len(X_train)))
                     print("epoch: {0}, batch: {1} / {2}".format(epoch, batch, train_n_batches))
                     summary, train_loss = sess.run([merged, loss], feed_dict={
-                            X: np.vstack(X_train_local[startIndex:endIndex].values).reshape(-1, 36, 36, 3),
+                            X: np.vstack(X_train_local[startIndex:endIndex].values).reshape(-1, 72, 72, 3),
                             y_: y_train_local[startIndex:endIndex].values,
                             is_training:True})
                     train_writer.add_summary(summary, trainStep)
                     print("loss: {}\n".format(train_loss))
 
                 summary, _ = sess.run([merged, train_step], feed_dict={
-                                    X: np.vstack(X_train_local[startIndex:endIndex].values).reshape(-1, 36, 36, 3),
+                                    X: np.vstack(X_train_local[startIndex:endIndex].values).reshape(-1, 72, 72, 3),
                                     y_: y_train_local[startIndex:endIndex].values,
                                     is_training:True})
                 train_writer.add_summary(summary, trainStep)
@@ -355,7 +355,7 @@ def main(X_train, X_test, y_train, y_test):
     print("TEST")
     test_loss = 0.0
     for i in range(len(X_test)):
-        test_df = get_local_data(X_test[i], y_test[i], 36)
+        test_df = get_local_data(X_test[i], y_test[i], 72)
         X_test_local = test_df["img_arr"]
         y_test_local = test_df["label"]
         test_n_batches = int(len(X_test_local) / batchSize)
@@ -364,7 +364,7 @@ def main(X_train, X_test, y_train, y_test):
             endIndex = startIndex + batchSize
 
             summary, tmp_loss = sess.run([merged, loss], feed_dict={
-                                X: np.vstack(X_test_local[startIndex:endIndex]).reshape(-1, 36, 36, 3),
+                                X: np.vstack(X_test_local[startIndex:endIndex]).reshape(-1, 72, 72, 3),
                                 y_: y_test_local[startIndex:endIndex].values,
                                 is_training:False})
             test_writer.add_summary(summary, testStep)
@@ -383,7 +383,7 @@ def main(X_train, X_test, y_train, y_test):
     label = label[:470, :]
     height = img.shape[0]
     width = img.shape[1]
-    df = get_local_data(img, label, 36)
+    df = get_local_data(img, label, 72)
     X_local = df["img_arr"]
     y_local = df["label"]
     estDensMap = np.zeros((height*width), dtype="float32")
@@ -393,7 +393,7 @@ def main(X_train, X_test, y_train, y_test):
         startIndex = batch*estBatchSize
         endIndex = startIndex + estBatchSize
         estDensMap[startIndex:endIndex] = sess.run(h_fc7, feed_dict={
-                        X: np.vstack(X_local[startIndex:endIndex]).reshape(-1, 36, 36, 3),
+                        X: np.vstack(X_local[startIndex:endIndex]).reshape(-1, 72, 72, 3),
                         is_training: False}).reshape(estBatchSize)
         print("DONE: batch:{}".format(batch))
 
