79ee0ab277cec6b4ceff8619d1877bffbfa4e072
kenya-sk <ksakka0309@gmail.com>
2018-04-21 14:28:23 +0900

    tmp

--- src/estimation.py
+++ src/estimation.py
@@ -21,41 +21,41 @@ def estimation(modelPath):
     y_ = tf.placeholder(tf.float32, [None, 1], name="label")
     is_training = tf.placeholder(tf.bool, name="is_training")
 
-    W_conv1 = cnn_pixel.weight_variable([7,7,3,32])
+    W_conv1 = tf.get_variable("conv1/weight1", shape=[7,7,3,32])
     conv1 = cnn_pixel.conv2d(X, W_conv1)
     conv1_bn = cnn_pixel.batch_norm(conv1, [0, 1, 2], 32, is_training)
     h_conv1 = tf.nn.leaky_relu(conv1_bn)
     h_pool1 = cnn_pixel.max_pool_2x2(h_conv1)
 
-    W_conv2 = cnn_pixel.weight_variable([7,7,32,32])
+    W_conv2 = tf.get_variable("conv2/weight2", shape=[7,7,32,32])
     conv2 = cnn_pixel.conv2d(h_pool1, W_conv2)
     conv2_bn = cnn_pixel.batch_norm(conv2, [0, 1, 2], 32, is_training)
     h_conv2 = tf.nn.leaky_relu(conv2_bn)
     h_pool2 = cnn_pixel.max_pool_2x2(h_conv2)
 
-    W_conv3 = cnn_pixel.weight_variable([5,5,32,64])
+    W_conv3 = tf.get_variable("conv3/weight3", shape=[5,5,32,64])
     conv3 = cnn_pixel.conv2d(h_pool2, W_conv3)
     conv3_bn = cnn_pixel.batch_norm(conv3, [0, 1, 2], 64, is_training)
     h_conv3 = tf.nn.leaky_relu(conv3_bn)
 
-    W_fc4 = cnn_pixel.weight_variable([18*18*64, 1000])
+    W_fc4 = tf.get_variable("fc4/weight4", shape[18*18*64, 1000])
     h_conv3_flat = tf.reshape(h_conv3, [-1, 18*18*64])
     fc4 = tf.matmul(h_conv3_flat, W_fc4)
     fc4_bn = cnn_pixel.batch_norm(fc4, [0], 1000, is_training)
     h_fc4 = tf.nn.leaky_relu(fc4_bn)
 
-    W_fc5 = cnn_pixel.weight_variable([1000, 400])
+    W_fc5 = tf.get_variable("fc5/weight5", shape=[1000, 400])
     fc5 = tf.matmul(h_fc4, W_fc5)
     fc5_bn = cnn_pixel.batch_norm(fc5, [0], 400, is_training)
     h_fc5 = tf.nn.leaky_relu(fc5_bn)
 
-    W_fc6 = cnn_pixel.weight_variable([400, 324])
+    W_fc6 = tf.get_variable("fc6/weight6", shape=[400, 324])
     fc6 = tf.matmul(h_fc5, W_fc6)
     fc6_bn = cnn_pixel.batch_norm(fc6, [0], 324, is_training)
     h_fc6 = tf.nn.leaky_relu(fc6_bn)
 
-    W_fc7 = cnn_pixel.weight_variable([324, 1])
-    b_fc7 = cnn_pixel.bias_variable([1])
+    W_fc7 = tf.get_variable("fc7/weight7", shape=[324, 1])
+    b_fc7 = tf.get_variable("fc7/bias7", shape=[1])
     y = tf.nn.leaky_relu(tf.matmul(h_fc6, W_fc7) + b_fc7)
     # -----------------------------------------------------------------
 
