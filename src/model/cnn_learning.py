299e47a758b52061411b5f7cb92df91f51645e09
kenya-sk <kenya0sakka@gmail.com>
2018-07-02 14:32:39 +0900

    Issue33: rename cnn_pixel.py -> cnn_learning.py

--- src/model/cnn_learning.py
+++ src/model/cnn_learning.py
@@ -126,7 +126,7 @@ def batch_norm(X, axes, shape, is_training):
 
 def main(X_train, X_test, y_train, y_test, modelPath):
     # start session
-    config = tf.ConfigProto(gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9))
+    config = tf.ConfigProto(gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5))
     sess = tf.InteractiveSession(config=config)
 
     # ------------------------------- MODEL -----------------------------------
